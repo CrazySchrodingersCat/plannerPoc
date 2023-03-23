08ce5d0960721c6b809da285ef7fa9ec77659318
CrazySchrodingersCat <kvitka911@gmail.com>
2023-03-23 15:41:10 +0100

    CORS
    8080 and 8081 added

--- API/Program.cs
+++ API/Program.cs
@@ -13,7 +13,9 @@ builder.Services.AddCors(options =>
     options.AddPolicy(name: MyAllowSpecificOrigins,
                       policy =>
                       {
-                          policy.WithOrigins("http://localhost:4200");
+                          policy.WithOrigins("http://localhost:4200",
+                              "http://localhost:8080",
+                               "http://localhost:8081");
                           policy.AllowAnyHeader();
                           policy.AllowAnyMethod();
                       });
