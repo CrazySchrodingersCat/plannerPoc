3eeeaac9998a1d22862ee4654d2987e806f1047d
CrazySchrodingersCat <kvitka911@gmail.com>
2023-04-24 10:56:51 +0200

    select user type added

--- AngularProject/src/app/component/plan/add-user/add-user.component.ts
+++ AngularProject/src/app/component/plan/add-user/add-user.component.ts
@@ -1,9 +1,17 @@
-import { AfterViewInit, Component, ViewChild } from '@angular/core';
+import {
+  AfterViewInit,
+  Component,
+  DoCheck,
+  OnChanges,
+  ViewChild,
+} from '@angular/core';
 import { MatPaginator } from '@angular/material/paginator';
 import { MatSort } from '@angular/material/sort';
 import { MatTableDataSource } from '@angular/material/table';
 import { Router } from '@angular/router';
+import { Client } from 'src/app/models/client.model';
 import { Practitioner } from 'src/app/models/practitioner.model';
+import { ClientService } from 'src/app/services/client.service';
 import { PractitionerService } from 'src/app/services/practitioner.service';
 
 @Component({
@@ -12,12 +20,12 @@ import { PractitionerService } from 'src/app/services/practitioner.service';
   styleUrls: ['./add-user.component.css'],
 })
 export class AddUserComponent implements AfterViewInit {
-  selectedUserType = '';
-  list!: Practitioner[];
+  selectedUserType = 'practitioner';
+  practitionerslist!: Practitioner[];
+  clientList!: Client[];
   displayedColumns: string[] = ['discipline', 'displayName'];
   dataSource = new MatTableDataSource<Practitioner>();
 
-  // @ViewChild(MatPaginator) paginator: MatPaginator;
   @ViewChild(MatSort) sort!: MatSort;
   @ViewChild(MatPaginator) paginator!: MatPaginator;
 
@@ -28,22 +36,41 @@ export class AddUserComponent implements AfterViewInit {
 
   constructor(
     private practitionerService: PractitionerService,
+    private clientService: ClientService,
     private router: Router
   ) {}
 
   ngOnInit(): void {
-    this.selectedUserType = 'practitioner';
-
     let practitionersFetch = this.practitionerService.getPractitioners();
     practitionersFetch.subscribe((practitioners: any) => {
-      this.list = practitioners;
+      this.practitionerslist = practitioners;
       this.dataSource.data = practitioners;
-      console.log(this.list);
+      console.log(this.practitionerslist);
     });
+    console.log(this.selectedUserType);
   }
 
   applyFilter(event: Event) {
     const filterValue = (event.target as HTMLInputElement).value;
     this.dataSource.filter = filterValue.trim().toLowerCase();
+  }
+  userTypeChanged() {
+    if (this.selectedUserType === 'client') {
+      console.log(this.selectedUserType);
+      this.displayedColumns = ['displayName'];
+      this.clientService.getAllClients().subscribe((clients: any) => {
+        this.practitionerslist = clients;
+        this.dataSource.data= clients;
+        console.log(this.practitionerslist);
+      });
+    }else{
+      let practitionersFetch = this.practitionerService.getPractitioners();
+      practitionersFetch.subscribe((practitioners: any) => {
+        this.practitionerslist = practitioners;
+        this.dataSource.data = practitioners;
+        console.log(this.practitionerslist);
+      });
+      console.log(this.selectedUserType);
+    }
   }
 }
