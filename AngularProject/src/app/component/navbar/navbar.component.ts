359389b5af7642cbb77c864446812ddc00de0201
CrazySchrodingersCat <kvitka911@gmail.com>
2023-04-25 15:43:40 +0200

    layout with sideBar

--- AngularProject/src/app/component/navbar/navbar.component.ts
+++ AngularProject/src/app/component/navbar/navbar.component.ts
@@ -1,10 +1,25 @@
 import { Component } from '@angular/core';
+import { NavigationEnd, Route, Router } from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
-  styleUrls: ['./navbar.component.css']
+  styleUrls: ['./navbar.component.css'],
 })
 export class NavbarComponent {
+  isPlanning = false;
+  currentRoute = '';
 
+  constructor(private router: Router) {
+    this.router.events.subscribe((val: any) => {
+      this.activateDiv(this.router.url);
+    });
+  }
+
+  activateDiv(url: string) {
+    // console.log(url == '/planner');
+    if (url == '/planner') {
+      this.isPlanning = true;
+    }
+  }
 }
