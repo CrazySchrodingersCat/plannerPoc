f365828d9ddd07e669622d7988a16b500e1a450c
CrazySchrodingersCat <kvitka911@gmail.com>
2023-04-27 13:11:03 +0200

    default day toggle added

--- AngularProject/src/app/component/navbar/navbar.component.ts
+++ AngularProject/src/app/component/navbar/navbar.component.ts
@@ -8,19 +8,35 @@ import { Router } from '@angular/router';
 })
 export class NavbarComponent {
   url: string = '';
+  selectedTimeToggle:number = 1;
 
   constructor(private router: Router) {
     this.router.events.subscribe((val: any) => {
       this.url = this.router.url;
     });
   }
+
+  // onInit(): void {
+  //   if (this.selectedTimeToggle == 1) {
+  //     const button = document.querySelector('.dayToggle');
+  //      if (button instanceof HTMLElement) {
+  //        button.classList.toggle('selectedButton');
+  //      }
+  //   }
+
+  // }
   dagToggeleClicked(event: any) {
-    event.target.classList.toggle('selectedButton');
+    this.selectedTimeToggle = 1;
+     event.target.classList.toggle('selectedButton');
+    
   }
   weekToggeleClicked(event: any) {
+    this.selectedTimeToggle = 2;
+    
     event.target.classList.toggle('selectedButton');
   }
   monthToggeleClicked(event: any) {
+    this.selectedTimeToggle = 3;
     event.target.classList.toggle('selectedButton');
   }
 }
--- AngularProject/src/app/component/plan/column/column.component.ts
+++ AngularProject/src/app/component/plan/column/column.component.ts
@@ -1,6 +1,7 @@
 import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { IUser } from 'src/app/models/IUser.model';
-
+import { AgendaItem } from 'src/app/models/agentaItem.model';
+import { AgendaService } from 'src/app/services/agenda.service';
 @Component({
   selector: 'app-column',
   templateUrl: './column.component.html',
@@ -10,14 +11,18 @@ export class ColumnComponent {
   @Input() currentDate = new Date();
   @Input() currentUser!: IUser;
   @Output() delete: EventEmitter<IUser> = new EventEmitter();
+  appointmentsList: AgendaItem[] = [];
 
   userType = '';
+  constructor(agendaService:AgendaService){}
 
   ngOnInit(): void {
     this.userType =
-      this.currentUser.discipline === ''
+      this.currentUser.discipline === undefined
         ? 'client'
         : this.currentUser.discipline!;
+        console.log(this.currentUser.discipline);
+        console.log(this.userType);
   }
   closeMe() {
      console.log('close clicked');
